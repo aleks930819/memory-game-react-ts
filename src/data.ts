191f89e5952c7c489b20d4bcf3c428c1b185d762
Aleksandar Atanasov <aleks930819@gmail.com>
2023-04-01 17:49:35 +0300

    added card data

---
+++ src/data.ts
@@ -0,0 +1,96 @@
+interface Card {
+  id: number;
+  name: string;
+  image: string;
+  flipped: boolean;
+  found: boolean;
+}
+
+const cards: Card[] = [
+  {
+    id: 1,
+    name: 'thor',
+    image: './assets/thor.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 2,
+    name: 'thor',
+    image: './assets/thor.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 3,
+    name: 'iron-man',
+    image: './assets/ironman.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 4,
+    name: 'iron-man',
+    image: './assets/ironman.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 5,
+    name: 'hawkeye',
+    image: './assets/hawkeye.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 6,
+    name: 'hawkeye',
+    image: './assets/hawkeye.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 7,
+    name: 'hulk',
+    image: './assets/hulk.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 8,
+    name: 'hulk',
+    image: './assets/hulk.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 9,
+    name: 'blackwidow',
+    image: './assets/blackwidow.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 10,
+    name: 'blackwidow',
+    image: './assets/blackwidow.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 11,
+    name: 'spiderman',
+    image: './assets/spiderman.png',
+    flipped: false,
+    found: false,
+  },
+  {
+    id: 12,
+    name: 'spiderman',
+    image: './assets/spiderman.png',
+    flipped: false,
+    found: false,
+  },
+];
+
+export default cards;
